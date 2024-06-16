6a0b3f4c5e2cf8b3f2b76b3c2af2664ecbe15894
Eshant Yadav <147942416+yeshant1@users.noreply.github.com>
2024-06-16 14:29:11 +0530

    Create Q13-FirstandLastOccuofElemInArr.cpp

---
+++ Q13-FirstandLastOccuofElemInArr.cpp
@@ -0,0 +1,63 @@
+//https://www.naukri.com/code360/problems/first-and-last-position-of-an-element-in-sorted-array_1082549
+#include <bits/stdc++.h> 
+int firstOcc(vector<int>& arr,int n,int key){
+
+    int s = 0;
+    int e = n-1;
+    int ans = -1;
+    int mid = s + (e-s)/2;
+
+    while(s<=e){
+
+        if(arr[mid] == key){
+            ans = mid;
+            e = mid-1;
+        }
+        else if(key > arr[mid]){
+            //right me jao
+            s = mid + 1;
+        }
+        else if(key < arr[mid]){
+            //left me jao
+            e = mid -1;
+        }
+        mid = s + (e - s)/2;
+    }
+    return ans;
+}
+
+
+int lastOcc(vector<int>& arr,int n,int key){
+
+    int s = 0;
+    int e = n-1;
+    int ans = -1;
+    int mid = s + (e-s)/2;
+
+    while(s<=e){
+
+        if(arr[mid] == key){
+            ans = mid;
+            s = mid+1;
+        }
+        else if(key > arr[mid]){
+            //right me jao
+            s = mid + 1;
+        }
+        else if(key < arr[mid]){
+            //left me jao 
+            e = mid -1;
+        }
+        mid = s + (e - s)/2;
+    }
+    return ans;
+}
+pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k)
+{
+    // Write your code here
+    pair<int,int> p;
+    p.first = firstOcc(arr,n,k);
+    p.second = lastOcc(arr,n,k);
+
+    return p;
+}
