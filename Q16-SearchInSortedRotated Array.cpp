d3fa5524eea351abad6b357280a55d77b78f78b9
Eshant Yadav <147942416+yeshant1@users.noreply.github.com>
2024-06-17 16:03:01 +0530

    Create Q16-SearchInSortedRotated Array.cpp

---
+++ Q16-SearchInSortedRotated Array.cpp
@@ -0,0 +1,112 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int search(vector<int>& arr, int n, int k) {
+    int low = 0, high = n - 1;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        //if mid points the target
+        if (arr[mid] == k) return mid;
+
+        //if left part is sorted:
+        if (arr[low] <= arr[mid]) {
+            if (arr[low] <= k && k <= arr[mid]) {
+                //element exists:
+                high = mid - 1;
+            }
+            else {
+                //element does not exist:
+                low = mid + 1;
+            }
+        }
+        else { //if right part is sorted:
+            if (arr[mid] <= k && k <= arr[high]) {
+                //element exists:
+                low = mid + 1;
+            }
+            else {
+                //element does not exist:
+                high = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    vector<int> arr = {7, 8, 9, 1, 2, 3, 4, 5, 6};
+    int n = 9, k = 1;
+    int ans = search(arr, n, k);
+    if (ans == -1)
+        cout << "Target is not present.\n";
+    else
+        cout << "The index is: " << ans << "\n";
+    return 0;
+}
+/*
+
+int  getPivot(vector<int>& arr,int n){
+    int s = 0;
+    int e = n-1;
+    int mid = s+ (e-s)/2;
+
+    while(s<e){
+        if(arr[mid] > arr[0]){
+            s = mid + 1;
+        }
+        else{
+            e = mid;
+        }
+        mid = s + (e-s)/2;
+    }
+    return s;
+}
+
+int binarySearch(vector<int>& arr, int s,int e, int key)
+{
+  int start = s;
+  int end = e;
+
+  int mid = start + (end - start) / 2;
+
+  while (start <= end)
+  {
+    if (arr[mid] == key)
+    {
+      return mid;
+    }
+    // go to right wala part
+    if (key > arr[mid ])
+    {
+      start = mid + 1;
+    }
+    else // go to left wala part when key < arr[mid]
+    { 
+      end = mid - 1;
+    }
+     mid =  start + (end - start) / 2;
+  }
+   return -1;
+}
+
+int search(vector<int>& arr, int n, int k)
+{
+    // Write your code here.
+    // Return the position of K in ARR else return -1.
+    int pivot = getPivot(arr,n);
+    if(k >= arr[pivot] && k < arr[n-1] ){
+        //Binary Seach on IInd Line
+        return binarySearch(arr,pivot,n-1,k);
+    }
+    else{
+        //Binary Search on Ist Line
+        return binarySearch(arr,0,pivot-1,k);
+    }
+
+
+}
+
+*/
