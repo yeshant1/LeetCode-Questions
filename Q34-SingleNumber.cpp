1ab66057487a01adaf371ddc4feda1443711dc64
Eshant Yadav <147942416+yeshant1@users.noreply.github.com>
2024-06-30 21:15:02 +0530

    Create Q34-SingleNumber.cpp

---
+++ Q34-SingleNumber.cpp
@@ -0,0 +1,13 @@
+//leetcode 136
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result= 0;
+        for(int num : nums){
+            result ^= num;
+
+        }
+        return result;
+        
+    }
+};
