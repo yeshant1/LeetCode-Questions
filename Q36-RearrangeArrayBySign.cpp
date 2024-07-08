fc5f4e0c8ed854e0279099045da5f2c3f6ef4eed
Eshant Yadav <147942416+yeshant1@users.noreply.github.com>
2024-07-08 19:32:28 +0530

    Create Q36-RearrangeArrayBySign.cpp

---
+++ Q36-RearrangeArrayBySign.cpp
@@ -0,0 +1,23 @@
+//leetcode - 2149
+
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> positive;
+        vector<int> negative;
+
+        for(int i = 0;i<nums.size();i++){
+            if(nums[i] > 0) positive.push_back(nums[i]);
+            else negative.push_back(nums[i]);
+        }
+
+      for(int i=0;i<nums.size()/2;i++){
+      
+        nums[2*i] = positive[i];
+        nums[2*i+1] = negative[i];
+     }
+
+     return nums;
+        
+    }
+};
