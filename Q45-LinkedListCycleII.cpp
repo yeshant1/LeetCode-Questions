919af51831d8d7e47517bac51a01f0a9d1ddf0bf
Eshant Yadav <147942416+yeshant1@users.noreply.github.com>
2024-07-19 18:27:19 +0530

    Create Q45-LinkedListCycleII.cpp

---
+++ Q45-LinkedListCycleII.cpp
@@ -0,0 +1,28 @@
+//leetcode -142
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if(head == null){
+            return null;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast!= null && fast.next != null){
+            
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
+                slow = head;
+
+                while(slow != fast){
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
