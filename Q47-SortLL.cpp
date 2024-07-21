3e1ef1624369b701be76c663ae397000fcfab9ed
Eshant Yadav <147942416+yeshant1@users.noreply.github.com>
2024-07-21 18:20:46 +0530

    Create Q47-SortLL.cpp

---
+++ Q47-SortLL.cpp
@@ -0,0 +1,22 @@
+//leetcode 148
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        vector<int> arr;
+
+        ListNode* temp = head;
+        while(temp != NULL){
+            arr.push_back(temp->val);
+            temp = temp->next;
+          }
+
+        sort(arr.begin(),arr.end());
+
+        temp = head;
+        for(int i = 0;i<arr.size();i++){
+            temp->val= arr[i];
+            temp = temp ->next;
+        }
+        return head;
+    }
+};
